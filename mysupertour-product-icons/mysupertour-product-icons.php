6b9e76177e9eb900b438e6cc5440aa97e5eaedda
krista172025 <ms.architektor@bk.ru>
2025-12-23 15:55:32 +0300

    s
    s

--- mysupertour-product-icons/mysupertour-product-icons.php
+++ mysupertour-product-icons/mysupertour-product-icons.php
@@ -13,12 +13,10 @@ define('MST_PI_VERSION', '3.0.0');
 define('MST_PI_PATH', plugin_dir_path(__FILE__));
 define('MST_PI_URL', plugin_dir_url(__FILE__));
 
-require_once MST_PI_PATH . 'includes/class-mst-product-icons.php';
 require_once MST_PI_PATH . 'includes/class-mst-product-icons-sync.php';
 require_once MST_PI_PATH . 'includes/class-mst-product-icons-filters.php';
 
 add_action('plugins_loaded', function() {
-    MST_Product_Icons::instance();
     MST_Product_Icons_Sync::instance();
     MST_Product_Icons_Filters::instance();
 }, 10);
