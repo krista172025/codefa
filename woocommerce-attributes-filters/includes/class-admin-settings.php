6fa94f0698ed5917afc42e683f61d33097d1b7f4
krista172025 <ms.architektor@bk.ru>
2025-12-24 13:13:48 +0300

    7
    7

--- woocommerce-attributes-filters/includes/class-admin-settings.php
+++ woocommerce-attributes-filters/includes/class-admin-settings.php
@@ -1,61 +1,70 @@
 <?php
 class Admin_Settings {
-    public static function init() {
+    private static $instance;
+
+    public static function instance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+            self::init_hooks();
+        }
+        return self::$instance;
+    }
+
+    private static function init_hooks() {
         add_action('admin_menu', [self::class, 'add_settings_page']);
         add_action('admin_init', [self::class, 'register_settings']);
     }
 
     public static function add_settings_page() {
         add_menu_page(
-            'Настройки фильтров',
-            'Фильтры',
-            'manage_options',
-            'wcaf-settings',
-            [self::class, 'render_settings_page'],
-            'dashicons-filter',
-            90
+            'WooCommerce Filter Settings',  // Заголовок страницы
+            'Фильтры',                      // Название в меню
+            'manage_options',               // Права доступа
+            'wcaf-settings',                // Уникальный slug для страницы
+            [self::class, 'render_settings_page'], // Callback для отображения
+            'dashicons-filter',             // Иконка
+            90                              // Позиция в меню
         );
     }
 
     public static function register_settings() {
-        register_setting('wcaf-settings-group', 'wcaf_active_filters');
+        register_setting('wcaf-settings-group', 'wcaf_active_filters'); // Регистрация опции для хранения фильтров
     }
 
     public static function render_settings_page() {
         $available_filters = [
             'pa_tour-type' => 'Тип тура',
-            'pa_duration' => 'Длительность тура',
+            'pa_duration' => 'Длительность',
             'pa_transport' => 'Транспорт',
         ];
+        $active_filters = get_option('wcaf_active_filters', []); ?>
 
-        $active_filters = get_option('wcaf_active_filters', []);
-
-        ?>
         <div class="wrap">
-            <h1>Настройки фильтров</h1>
+            <h1><?php echo esc_html('Настройки фильтров'); ?></h1>
             <form method="post" action="options.php">
                 <?php settings_fields('wcaf-settings-group'); ?>
                 <?php do_settings_sections('wcaf-settings-group'); ?>
 
                 <table class="form-table">
-                <?php foreach ($available_filters as $taxonomy => $label): ?>
-                    <tr>
-                        <th scope="row">
-                            <label for="wcaf_<?php echo $taxonomy; ?>"><?php echo esc_html($label); ?></label>
-                        </th>
-                        <td>
-                            <input type="checkbox" name="wcaf_active_filters[]" value="<?php echo esc_attr($taxonomy); ?>"
-                                <?php checked(in_array($taxonomy, $active_filters)); ?>>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
+                    <?php foreach ($available_filters as $taxonomy => $label): ?>
+                        <tr>
+                            <th scope="row">
+                                <label for="wcaf_<?php echo esc_attr($taxonomy); ?>">
+                                    <?php echo esc_html($label); ?>
+                                </label>
+                            </th>
+                            <td>
+                                <input type="checkbox" name="wcaf_active_filters[]" value="<?php echo esc_attr($taxonomy); ?>"
+                                    <?php checked(in_array($taxonomy, (array) $active_filters, true)); ?>>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
                 </table>
-
-                <?php submit_button(); ?>
+                <?php submit_button('Сохранить настройки'); ?>
             </form>
         </div>
         <?php
     }
 }
 
-Admin_Settings::init();
+Admin_Settings::instance();
--- woocommerce-attributes-filters/woocommerce-attributes-filters.php
+++ woocommerce-attributes-filters/woocommerce-attributes-filters.php
@@ -6,20 +6,20 @@
  * Author: Your Name
  */
 
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) exit; // Предотвращение прямого доступа
 
 define('WCAF_VERSION', '1.0.0');
 define('WCAF_PATH', plugin_dir_path(__FILE__));
 define('WCAF_URL', plugin_dir_url(__FILE__));
 
+// Подключение основных файлов классов
 require_once WCAF_PATH . 'includes/class-attributes-filters.php';
 require_once WCAF_PATH . 'includes/class-ajax-handler.php';
+require_once WCAF_PATH . 'includes/class-admin-settings.php';
 
-
+// Инициализация плагина
 add_action('plugins_loaded', function() {
-    Attributes_Filters::instance();
-    Ajax_Handler::instance(); // Исправляем вызов экземпляра на корректный
-});
-add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_style('wcaf-filters-liquid-glass', plugins_url('assets/css/filters.css', __FILE__), [], '1.1');
+    Attributes_Filters::instance(); // Инициализация класса фильтров
+    Ajax_Handler::instance();       // Инициализация класса обработки запросов
+    Admin_Settings::instance();     // Инициализация класса настроек админки
 });
